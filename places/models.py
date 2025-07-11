60d234e6e62bcad3cbc0db3f8a540aa7a8d311e6
karaman56 <166434324+karaman56@users.noreply.github.com>
2025-07-12 02:23:34 +0500

    created models
    created models

--- places/models.py
+++ places/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.db import models
 
-print("Hello")
+
+class Place(models.Model):
+  title = models.CharField("Название места", max_length=255)
+  description_short = models.TextField("Краткое описание")
+  description_long = models.TextField("Подробное описание")
+  lat = models.FloatField("Широта")
+  lng = models.FloatField("Долгота")
+
+  def __str__(self):
+    return self.title
