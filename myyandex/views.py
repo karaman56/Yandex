d0563fc4d50a41efdb36ad25fb6a3a312125e075
karaman56 <166434324+karaman56@users.noreply.github.com>
2025-07-13 02:19:46 +0500

    Move data from template to view
     Move data from template to view

--- myyandex/views.py
+++ myyandex/views.py
@@ -1,15 +1,54 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-from django.http import JsonResponse  # Добавьте этот импорт
+from places.models import Place, Image
 import json
-import os
-from django.conf import settings
 
 
 def index(request):
-  return render(request, 'myyandex/index.html')
+  places = Place.objects.all()
+  geojson = {
+    "type": "FeatureCollection",
+    "features": []
+  }
+
+  for place in places:
+    feature = {
+      "type": "Feature",
+      "geometry": {
+        "type": "Point",
+        "coordinates": [place.lng, place.lat]
+      },
+      "properties": {
+        "title": place.title,
+        "placeId": place.id,
+        "detailsUrl": "/place-details/"
+      }
+    }
+    geojson["features"].append(feature)
+
+  places_json = json.dumps(geojson, ensure_ascii=False, indent=2)
+  return render(request, 'myyandex/index.html', {'places_json': places_json})
+
 
 def place_details(request):
   place_id = request.GET.get('place_id')
-  path = f"myyandex/static/myyandex/places/{place_id}.json"
-  return JsonResponse(json.load(open(path, 'r', encoding='utf-8')))
+
+  if not place_id:
+    return JsonResponse({'error': 'Missing place_id parameter'}, status=400)
+
+  try:
+    place_id = int(place_id)
+    place = Place.objects.get(id=place_id)
+    images = place.images.all().order_by('position')
+    place_data = {
+      'title': place.title,
+      'imgs': [request.build_absolute_uri(image.image.url) for image in images],
+      'description_short': place.description_short,
+      'description_long': place.description_long,
+    }
+    return JsonResponse(place_data)
+  except (ValueError, TypeError):
+    return JsonResponse({'error': 'Invalid place_id format'}, status=400)
+  except Place.DoesNotExist:
+    return JsonResponse({'error': 'Place not found'}, status=404)
 
