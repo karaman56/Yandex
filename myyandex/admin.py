0c9924eead54d7d43f66f2f367a0f78bc85968f8
karaman56 <166434324+karaman56@users.noreply.github.com>
2025-07-19 00:49:55 +0500

    Загружать картинки можно прямо на странице локации
    Загружать картинки можно прямо на странице локации

--- myyandex/admin.py
+++ myyandex/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
 from places.models import Place, Image
 
-admin.site.register(Place)
-admin.site.register(Image)
+
+
--- places/admin.py
+++ places/admin.py
@@ -2,10 +2,34 @@ from django.contrib import admin
 from .models import Place, Image
 from django.utils.html import format_html
 
+
+class ImageInline(admin.TabularInline):
+  model = Image
+  extra = 3
+  fields = ('image', 'position', 'preview')
+  readonly_fields = ('preview',)
+
+  def preview(self, obj):
+    if obj.image:
+      return format_html('<img src="{}" height="150">', obj.image.url)
+    return "Превью"
+
+
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ['title', 'lat', 'lng']
-    search_fields = ['title']
+  inlines = [ImageInline]
+  list_display = ['title', 'lat', 'lng']
+  search_fields = ['title']
 
 
 class ImageAdmin(admin.ModelAdmin):
-  list_display = ('place', 'image')
+  list_display = ('place', 'preview')
+
+  def preview(self, obj):
+    if obj.image:
+      return format_html('<img src="{}" height="50">', obj.image.url)
+    return "Нет изображения"
+
+
+
+admin.site.register(Place, PlaceAdmin)
+admin.site.register(Image, ImageAdmin)
