e55141dedd367fa3b5bddc08690f6f1cc692e1bb
karaman56 <166434324+karaman56@users.noreply.github.com>
2025-07-19 01:29:34 +0500

    фото можно перетаскивать прям в  админике
    фото можно перетаскивать прям в  админике

--- mysite/settings.py
+++ mysite/settings.py
@@ -22,6 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'myyandex.apps.MyyandexConfig',
     'places.apps.PlacesConfig',
+    'adminsortable2',
 ]
 
 MIDDLEWARE = [
--- places/admin.py
+++ places/admin.py
@@ -1,35 +1,27 @@
 from django.contrib import admin
 from .models import Place, Image
 from django.utils.html import format_html
+from adminsortable2.admin import SortableInlineAdminMixin, SortableAdminBase
 
+class ImageInline(SortableInlineAdminMixin, admin.TabularInline):
+    model = Image
+    extra = 3
+    fields = ('image', 'preview')
+    readonly_fields = ('preview',)
 
-class ImageInline(admin.TabularInline):
-  model = Image
-  extra = 3
-  fields = ('image', 'position', 'preview')
-  readonly_fields = ('preview',)
+    def preview(self, obj):
+        if obj.image:
+            return format_html('<img src="{}" height="150">', obj.image.url)
+        return "Превью"
 
-  def preview(self, obj):
-    if obj.image:
-      return format_html('<img src="{}" height="150">', obj.image.url)
-    return "Превью"
+# Наследуем PlaceAdmin от SortableAdminBase
+@admin.register(Place)
+class PlaceAdmin(SortableAdminBase, admin.ModelAdmin):
+    inlines = [ImageInline]
+    list_display = ['title', 'lat', 'lng']
+    search_fields = ['title']
 
-
-class PlaceAdmin(admin.ModelAdmin):
-  inlines = [ImageInline]
-  list_display = ['title', 'lat', 'lng']
-  search_fields = ['title']
-
-
+@admin.register(Image)
 class ImageAdmin(admin.ModelAdmin):
-  list_display = ('place', 'preview')
-
-  def preview(self, obj):
-    if obj.image:
-      return format_html('<img src="{}" height="50">', obj.image.url)
-    return "Нет изображения"
-
-
-
-admin.site.register(Place, PlaceAdmin)
-admin.site.register(Image, ImageAdmin)
+    list_display = ('__str__', 'place', 'position')
+    list_editable = ('position',)
